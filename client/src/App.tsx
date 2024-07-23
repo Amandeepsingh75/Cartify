80a85e9618f51d2172146e5af23b49ea80dd6a85
Amandeep75 <Amandeep75318gmail.com>
2024-07-23 12:30:07 +0530

    commit env change and lint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import Login from "./components/Login"
-import {BrowserRouter, Routes , Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Register from "./components/Registeration"
 import Home from "./components/Home"
 import AddProducts from "./components/products/AddProducts"
@@ -9,15 +9,15 @@ import ProtectiveRotes from "./components/ProtectiveRotes"
 function App() {
   return (
     <>
-      
+
       <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Home />}/>
-        <Route  path="/register" element={<Register />}/>
-        <Route  path="/login" element={<Login />}/>
-          <Route path="/add-product" element={<ProtectiveRotes Component={<AddProducts />} />}/>
-        <Route path="*" element={<Error404 />}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/add-product" element={<ProtectiveRotes Component={<AddProducts />} />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
       </BrowserRouter>
     </>
   )
